92a14a62920d9f76b2ab411de0494f97a1987c16
Mirza Ali Baig <mirzaalibaig0714@gmail.com>
2024-12-09 19:01:58 +0500

    C Sharp : Class 13

---
+++ C Sharp/ConsoleApps/Class_12/Program.cs
@@ -0,0 +1,86 @@
+﻿using System.IO;
+
+namespace Class_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // !!!!!!!!!!!!!!!!!!! Class 12 !!!!!!!!!!!!!!!!!!!!!!!
+            // !!!!!!!!!!!!!!!!!!! Topics !!!!!!!!!!!!!!!!!!!!!!!
+            // File Handling in c#
+            // Exists
+            // Create
+            // Read
+            // Move
+            // Copy
+            // Write
+            // Delete
+            //Console.Write("Somthing");   
+            //Console.Write("Somthing Else");
+
+            string path = @"C:\Users\Admin\Documents\demo.txt";
+            string path2 = @"C:\Users\Admin\Desktop\demo.txt";
+
+            try
+            {
+                if (File.Exists(path))
+                {
+                    Console.WriteLine("File Found");
+
+                }
+                else
+                {
+                    File.Create(path);
+                    Console.WriteLine("File Not Found");
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message); ;
+            }
+
+            if (File.Exists(path))
+            {
+               string[] fileText=  File.ReadAllLines(path);
+                //string fileText=  File.ReadAllText(path);
+
+                foreach (string text in fileText)
+                {
+                    Console.WriteLine(text + "\n");
+                }
+
+            }
+
+            if (File.Exists(path)){
+                File.Move(path, path2,true);
+              
+            }
+
+            if(File.Exists(path2)) {
+                    File.Copy(path2 , path,true);            
+            }
+            
+
+            if(File.Exists(path2))
+            {
+                File.WriteAllText(path2, "Write Using C#");
+
+                File.AppendAllText(path2, "\n2nd Line");
+            }
+
+
+
+            if (File.Exists(path2))
+            {
+              File.Delete(path2);
+            }
+
+
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ C Sharp/ConsoleApps/Class_13/Program.cs
@@ -0,0 +1,98 @@
+﻿using System.Net.Http;
+
+namespace Class_13
+{
+    internal class Program
+    {
+        static void  Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            string url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+            //try
+            //{
+
+            //    HttpClient client = new HttpClient();
+            //    HttpResponseMessage res = await client.GetAsync(url);
+
+            //    if (res.IsSuccessStatusCode)
+            //    {
+
+            //        var data = await res.Content.ReadAsStringAsync();
+            //        Console.WriteLine(data);
+            //        //return data.ToString();
+            //    } 
+            //    else
+            //    {
+            //        Console.WriteLine("Error");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine("Error");
+            //    Console.WriteLine(ex.Message);
+            //    //return "";
+            //}
+            APICall(url);
+            Person person1 = new Person(20,"Hamza");
+            person1.Greeting();
+
+        }
+        static async void APICall(string url)
+        {
+
+            try
+            {
+
+                HttpClient client = new HttpClient();
+                HttpResponseMessage res = await client.GetAsync(url);
+
+                if (res.IsSuccessStatusCode)
+                {
+
+                var data= await res.Content.ReadAsStringAsync();
+                Console.WriteLine(data);
+                    //return data.ToString();
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error");
+                Console.WriteLine(ex.Message);
+                //return "";
+            }
+        }
+    }
+
+    struct Person
+    {
+        // Properties
+        // Methods
+        // Constructor
+
+        public string Name { get; set; }
+        public readonly int age = 20;
+
+        public Person(int age,string name)
+        {
+            this.age = age;
+            this.Name = name;
+        }
+
+        public void Greeting()
+        {
+            Console.WriteLine("Hello Welcome "+ Name);
+        }
+
+        //public void setAge(int age)
+        //{
+        //    this.age = age;
+        //}
+
+
+    }
+}
